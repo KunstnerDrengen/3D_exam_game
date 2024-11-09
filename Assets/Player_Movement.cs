12eeb55b5a3531d9b68a53a89144162d7344dbb6
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2024-11-09 14:17:40 +0100

    updated

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -1,22 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class Player_Movement : MonoBehaviour
 {
 
     public CharacterController controller; 
 
-    public float playerspeed = 12f;
+    public float playerspeed;
+    public float targetTime = 5.0f;
+
+    public bool tired = false;
 
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
+        bool sprinter = Input.GetKey(KeyCode.LeftShift);
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move *playerspeed * Time.deltaTime);
+
+        if ((sprinter == true && targetTime > 0.0f))
+        {
+          playerspeed = 6f;
+          targetTime -= Time.deltaTime;
+          if (targetTime <= 0)
+            {
+                targetTime = 0.0f;
+                Debug.Log("tired");
+            }
+        }
+        else if (sprinter == false)
+        {
+            playerspeed = 3f;
+            targetTime += Time.deltaTime;
+            if (targetTime >= 5)
+            {
+                targetTime = 5.0f;
+                Debug.Log("full");
+            }
+        }
     }
 }
