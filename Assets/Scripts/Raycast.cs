fa1c246a15de4f846999e7208578143381e058fb
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2024-11-20 15:53:18 +0100

    key update

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -2,12 +2,35 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour
 {
+    public int Keyccount = 0;
     void FixedUpdate()
     {
+        bool isInteract = Input.GetKey(KeyCode.E);
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
-        if (Physics.Raycast(transform.position, fwd, 10))
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, fwd * 3, Color.red);
+
+        // Perform the raycast and check for a hit within 10 units
+        if (Physics.Raycast(transform.position, fwd, out hit, 10))
+        {
             print("There is something in front of the object!");
-        
+
+            // Check if the e key is pressed
+            if (isInteract)
+            {
+                Debug.Log("huha");
+                // Check if the object in front has the tag "key"
+                if (hit.collider.CompareTag("Key"))
+                {
+                    // Destroy the object with the "key" tag
+                    Destroy(hit.collider.gameObject);
+
+                    print("Key object destroyed!");
+                    Keyccount = Keyccount + 1;
+                }
+            }
+        }
     }
 }
+
