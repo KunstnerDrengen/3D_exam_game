ba010c78cd9b0b0d99a6ab50070de4521df3c2e6
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2024-11-20 16:33:54 +0100

    pik

--- Assets/Scripts/Keycount.cs
+++ Assets/Scripts/Keycount.cs
@@ -7,28 +7,11 @@ public class DisplayInt : MonoBehaviour
     public int number; // The integer you want to display
     public Text displayText; // Reference to the Text component
 
-    void Start()
-    {
-        raycasting = GetComponent<Raycast>();
-        UpdateText(); // Update the text initially
-
-    }
-
     void Update()
     {
         number = raycasting.Keyccount; 
-    }
-
-    void UpdateText()
-    {
-        displayText.text = number.ToString(); // Convert the integer to a string and display it
-    }
-
-    // Optionally, you can update the number and refresh the display dynamically
-    public void UpdateNumber(int newNumber)
-    {
-        number = newNumber;
-        UpdateText();
+        raycasting = GetComponent<Raycast>();
+        displayText.text = number.ToString();
     }
 }
 
